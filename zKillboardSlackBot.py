3645dd9156c738884be2f3dd5723cb077658ac64
Cameronkeene15 <Cameronkeene15@gmail.com>
2016-05-17 00:40:44 -0400

    Added in Config.ini which is where the settings will be

--- zKillboardSlackBot.py
+++ zKillboardSlackBot.py
@@ -5,87 +5,41 @@ import pprint
 import csv
 import re
 import configparser
+import sys
 
 # for fixLazyJson
 import tokenize
 import token
 from io import *
 
-# change this to your webhook for the slack channel that you want kills posted to.
-slack_web_hook = 'https://hooks.slack.com/services/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'
-# slack_web_hook = 'https://hooks.slack.com/services/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'
-
-# change these to your corp id and alliance id. ONLY ALLIANCE ID WORKS RIGHT NOW!
-# TO DO: fix these so that only one is required, will probably do this as modes once a config file is incorporated.
-alliance_id = 99004364
-corporation_id = 98380820
-
-# Change this to a recent kill that has occured on your alliance or corp killboard>
-# It is used so that at first run when there is no recent_kill_id_list.csv, it will not pull the last 200 kills from
-# zKillboard
-recent_kill = 53953253
-
 
 def main():
-    cache_size = 10
-    data = DataHandler(cache_size)
+    config = ConfigHandler()
+    data = DataHandler(config)
     data.read_kill_list_file()
     web_handler = WebHandler()
-    url = web_handler.generate_zkillboard_url(data)
+    url = web_handler.generate_zkillboard_url(data, config)
     reader = codecs.getreader('utf-8')
     response = json.load(reader(urllib.request.urlopen(url)))
+    # Used for testing. To save Json data from zKillboard, then read from it when testing.
+    # Helps to not spam request to zKillboard
     #    with open('json_info.txt', 'w') as json_file:
     #        json_file.write(pprint.pformat(response))
     #     with open('json_info.txt', 'r') as json_file:
     #            response = json.loads(fixLazyJson(json_file.read()))
     if_new_kill = False
-    config_test = ConfigHandler()
-    config_test.generate_config_file()
     for kill_mail in response:
         kill = KillMail(kill_mail)
         if data.check_if_new_kill(kill):
             if_new_kill = True
             pprint.pformat(kill_mail)
             web_handler.get_html_page(kill)
-            slack_message = SlackMessage(kill, web_handler)
+            slack_message = SlackMessage(kill, web_handler, config)
             encoded_slack_message = slack_message.encode_slack_message()
-            req = urllib.request.urlopen(slack_web_hook, encoded_slack_message)
-            page = req.read()
-            print(page)
+            urllib.request.urlopen(config.get_slack_web_hook(), encoded_slack_message)
             data.add_kill_id(kill.get_kill_id())
     if if_new_kill:
         data.write_kill_list_file()
-
-
-def fixLazyJson(in_text):
-    tokengen = tokenize.generate_tokens(StringIO(in_text).readline)
-
-    result = []
-    for tokid, tokval, _, _, _ in tokengen:
-        # fix unquoted strings
-        if (tokid == token.NAME):
-            if tokval not in ['true', 'false', 'null', '-Infinity', 'Infinity', 'NaN']:
-                tokid = token.STRING
-                tokval = u'"%s"' % tokval
-
-        # fix single-quoted strings
-        elif (tokid == token.STRING):
-            if tokval.startswith("'"):
-                tokval = u'"%s"' % tokval[1:-1].replace('"', '\\"')
-
-        # remove invalid commas
-        elif (tokid == token.OP) and ((tokval == '}') or (tokval == ']')):
-            if (len(result) > 0) and (result[-1][1] == ','):
-                result.pop()
-
-        # fix single-quoted strings
-        elif (tokid == token.STRING):
-            if tokval.startswith("'"):
-                tokval = u'"%s"' % tokval[1:-1].replace('"', '\\"')
-
-        result.append((tokid, tokval))
-
-    return tokenize.untokenize(result)
 
 
 class KillMail:
@@ -162,49 +116,35 @@ class KillMail:
 
 
 class SlackMessage:
-    def __init__(self, kill, web_handler):
+    def __init__(self, kill, web_handler, config):
         self.kill = kill
         self.web_handler = web_handler
-
-    def generate_message_title(self):
-        victim = self.kill.get_victim_character_name()
-        corporation = self.kill.get_victim_corporation_name()
-        ship_name = self.kill.get_ship_name()
-
-        title = victim + '(' + corporation + ') lost their ' + ship_name
-        return title
+        self.config = config
 
     def get_kill_link(self):
         kill_link = 'https://zkillboard.com/kill/' + str(self.kill.get_kill_id())
         return kill_link
 
     def get_message_color(self):
-        if alliance_id == self.kill.get_victim_alliance_id() or corporation_id == self.kill.get_victim_corporation_id():
+        if self.config.get_alliance_id() == self.kill.get_victim_alliance_id() or self.config.get_corporation_id() == self.kill.get_victim_corporation_id():
             color = '#ff0000'
         else:
             color = '#36a64f'
         return color
 
     def get_message_icon_emoji(self):
-        if alliance_id == self.kill.get_victim_alliance_id() or corporation_id == self.kill.get_victim_corporation_id():
+        if self.config.get_alliance_id() == self.kill.get_victim_alliance_id() or self.config.get_corporation_id() == self.kill.get_victim_corporation_id():
             icon_emoji = ':skull_and_crossbones:'
         else:
             icon_emoji = ':sunglasses:'
         return icon_emoji
 
     def get_message_user_name(self):
-        if alliance_id == self.kill.get_victim_alliance_id() or corporation_id == self.kill.get_victim_corporation_id():
+        if self.config.get_alliance_id() == self.kill.get_victim_alliance_id() or self.config.get_corporation_id() == self.kill.get_victim_corporation_id():
             user_name = 'Loss'
         else:
             user_name = 'Kill'
         return user_name
-
-    def get_thumb_nail_url(self):
-        url = 'https://image.eveonline.com/Render/'
-        url += str(self.kill.get_ship_id())
-        url += '_64.png'
-        print(url)
-        return url
 
     # Old format, decided to use V2 instead because it takes up less space and fields does not repeat the title.
     def generate_slack_message(self):
@@ -250,6 +190,8 @@ class SlackMessage:
         }
         return slack_message
 
+# Took out repetitive info so now just descriptive title with link to kill and total value are shown.
+# Makes the killmails posted in slack much more compact and easier to view.
     def generate_slack_message_v2(self):
         slack_message = {
             "username": self.get_message_user_name(),
@@ -279,15 +221,12 @@ class SlackMessage:
 
 
 class DataHandler:
-    def __init__(self, cache_size):
+    def __init__(self, config):
         self.kill_list = []
-        self.cache_size = cache_size
-
-    def set_cache_size(self, cache_size):
-        self.cache_size = cache_size
+        self.config = config
 
     def get_cache_size(self):
-        return self.cache_size
+        return self.config.get_cache_size()
 
     def get_kill_list(self):
         return self.kill_list
@@ -313,7 +252,7 @@ class DataHandler:
             with open('recent_kill_list.csv', 'w', newline='') as kill_list_file:
                 writer = csv.writer(kill_list_file, delimiter='\n')
                 self.kill_list.sort(reverse=True)
-                while len(self.kill_list) > self.cache_size:
+                while len(self.kill_list) > self.config.get_cache_size():
                     print('removing: ' + str(self.kill_list[-1]))
                     del self.kill_list[-1]
                 writer.writerow(self.kill_list)
@@ -326,12 +265,12 @@ class DataHandler:
                 file_reader = csv.reader(kill_list_file, delimiter='\n')
                 for kill_id in file_reader:
                     self.kill_list.append(kill_id[0])
-                # Needed to convert the Items in the list from Strings to Ints because csv.reader returns a
+                # Needed to convert the items in the list from Strings to Ints because csv.reader returns a
                 # list of Strings
                 self.kill_list = list(map(int, self.kill_list))
         except FileNotFoundError:
-            print('recent_kill_id_list.csv now found\nUsing recent_kill\n')
-            self.kill_list = [recent_kill]
+            print('recent_kill_id_list.csv not found\nUsing recent_kill\n')
+            self.kill_list = [self.config.get_recent_kill()]
 
 
 class WebHandler:
@@ -364,9 +303,9 @@ class WebHandler:
         description_list = description_regex.findall(self.html)
         self.description = self.remove_total_value_from_title(description_list[0])
 
-    def generate_zkillboard_url(self, data):
+    def generate_zkillboard_url(self, data, config):
         url = 'https://zkillboard.com/api/'
-        url += 'allianceID/' + str(alliance_id) + '/'
+        url += 'allianceID/' + str(config.get_alliance_id()) + '/'
         url += 'afterKillID/' + str(min(data.get_kill_list())) + '/'
         url += 'no-items/no-attackers/'
         print(url)
@@ -375,21 +314,83 @@ class WebHandler:
     def remove_total_value_from_title(self, title):
         if ' Total Value:' in title:
             title = title[:title.find(' Total Value:')]
-        print(title)
         return title
 
 
 class ConfigHandler:
     def __init__(self):
         self.config = configparser.ConfigParser()
+        self.read_config_file()
 
     def generate_config_file(self):
-        self.config['Settings'] = {'alliance_id': '',
-                                   'corporation_id': '',
-                                   'slack_web_hook': '',
-                                   'recent_kill_id': ''}
-        with open('Config.ini', 'w') as configfile:
-            self.config.write(configfile)
+        self.config.add_section('Settings')
+        self.config.set('Settings', 'slack_web_hook', 'https://hooks.slack.com/services/')
+        self.config.set('Settings', 'alliance_id', '')
+        self.config.set('Settings', 'corporation_id', '')
+        self.config.set('Settings', 'cache_size', '10')
+        self.config.set('Settings', 'recent_kill_id', '')
+        try:
+            with open('Config.ini', 'w') as config_file:
+                self.config.write(config_file)
+        except:
+            print('Error Generating Config File!')
 
+    def read_config_file(self):
+        try:
+            with open('Config.ini') as config_file:
+                self.config.read_file(config_file)
+        except FileNotFoundError:
+            print('Could Not Find Config File\nGenerating Config File Now')
+            self.generate_config_file()
+            print('Config File Now Available! Go Configure It!')
+            sys.exit()
+        except:
+            print('Error Reading Config File!')
+
+    def get_slack_web_hook(self):
+        return self.config.get('Settings', 'slack_web_hook')
+
+    def get_alliance_id(self):
+        return self.config.get('Settings', 'alliance_id')
+
+    def get_corporation_id(self):
+        return self.config.get('Settings', 'corporation_id')
+
+    def get_cache_size(self):
+        return int(self.config.get('Settings', 'cache_size'))
+
+    def get_recent_kill(self):
+        return int(self.config.get('Settings', 'recent_kill_id'))
+
+# Only used for testing to fix reading json data from saved file.
+def fixLazyJson(in_text):
+    tokengen = tokenize.generate_tokens(StringIO(in_text).readline)
+
+    result = []
+    for tokid, tokval, _, _, _ in tokengen:
+        # fix unquoted strings
+        if (tokid == token.NAME):
+            if tokval not in ['true', 'false', 'null', '-Infinity', 'Infinity', 'NaN']:
+                tokid = token.STRING
+                tokval = u'"%s"' % tokval
+
+        # fix single-quoted strings
+        elif (tokid == token.STRING):
+            if tokval.startswith("'"):
+                tokval = u'"%s"' % tokval[1:-1].replace('"', '\\"')
+
+        # remove invalid commas
+        elif (tokid == token.OP) and ((tokval == '}') or (tokval == ']')):
+            if (len(result) > 0) and (result[-1][1] == ','):
+                result.pop()
+
+        # fix single-quoted strings
+        elif (tokid == token.STRING):
+            if tokval.startswith("'"):
+                tokval = u'"%s"' % tokval[1:-1].replace('"', '\\"')
+
+        result.append((tokid, tokval))
+
+    return tokenize.untokenize(result)
 
 main()
